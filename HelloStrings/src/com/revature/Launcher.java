51d9411a9c63d10f2ea781ecaedb44fc67198e70
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-07-03 12:33:20 -0400

    finished string demo, saw methods and StringBuilder

--- HelloStrings/src/com/revature/Launcher.java
+++ HelloStrings/src/com/revature/Launcher.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Launcher {
 
     public static void main(String[] args){
@@ -41,7 +43,7 @@ public class Launcher {
 
         //These two String are technically the same object in memory, since we made them as String literals
 
-        //by using the new keyword, we will ALWAYS createa new object in memory
+        //by using the new keyword, we will ALWAYS create a new object in memory
         String st3 = new String("Hello Java");
 
         System.out.println(st == st2); //true - but not why you might think. It's comparing MEMORY ADDRESS
@@ -50,6 +52,77 @@ public class Launcher {
         //THIS is exactly why we must use .equals() for objects - to compare VALUE not MEMORY ADDRESS
         System.out.println(st.equals(st3));
 
+        //and yes, even non string-pool strings are immutable
+        st3.toUpperCase();
+        System.out.println("st3: " + st3);
+
+        System.out.println("======================(String Methods)");
+
+        //a pangram to use some String methods on\
+        String pangram = "The five boxing wizards jump quickly.";
+
+        //we can use the .length() method to return an int representing the length of the String
+        System.out.println("The pangram is: " + pangram.length() + " chars long");
+
+        //.subString() returns a new String found in the original String
+        System.out.println(pangram.substring(10)); //print out the pangram from index 10 and on
+        System.out.println(pangram.substring(10, 35)); //print out the pangram from index 10 to 35
+
+        //we can also use .contains to see if a String contains a certain substring
+        if(pangram.contains("wizard")){
+            System.out.println("The pangram has something to do with Wizards probably");
+        }
+
+        //we can use .split() to create an Array of Strings based on a certain delimiter
+        String[] words = pangram.split(" "); //every space in the pangram will indicate a unique String in the Array
+
+        //let's print out the Array using an enhanced for loop
+        System.out.println(words);
+
+        for(String word : words){
+            System.out.println(word);
+        }
+
+        //.chatAt returns the character in a String at whatever index we ask for
+        System.out.println(pangram.charAt(5)); //prints 'v'
+
+        //"The five boxing wizards jump quickly."
+
+        //a common use of charAt is reversing a String
+        String reversed = "";
+        for(int j = pangram.length() - 1; j >= 0; j--){
+            reversed += pangram.charAt(j);
+        }
+        System.out.println(reversed);
+
+        //we can use .startsWith() and .endsWith() to check if a String starts or ends with a certain substring
+        System.out.println(pangram.startsWith("T"));
+        System.out.println(pangram.startsWith("The"));
+        //both true!
+
+        System.out.println("============(Quick example of StringBuilder)");
+
+        //"The five boxing wizards jump quickly."
+
+        //StringBuilder is a Class meant to emulate a String that is MUTABLE (can be change)
+        StringBuilder sb = new StringBuilder("I'm a StringBuilder");
+
+        //.reverse() lets us reverse the characters super easily
+        sb.reverse();
+
+        //.append() lets us add to the end of the StringBuilder
+        sb.append("(I WAS ADDED AFTER THE FACT!!!)");
+
+        //.replace() lets us replace a substring with another substring
+        sb.replace(4, 10, "(I HAVE BEEN REPLACED)");
+
+        //we could have also chained these, I find it messy
+        //sb.reverse().append("(I WAS ADDED AFTER THE FACT!!!)").replace(4, 10, "(I HAVE BEEN REPLACED)");
+
+        //print it out to see the modifications
+        System.out.println(sb);
+
     }
+
 
 }
