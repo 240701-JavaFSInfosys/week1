ae26ba3c89d0fb009238978b67938c544187669b
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-07-05 14:30:30 -0400

    seen all 4 scopes, contrasted static/nonstatic variables

--- HelloScopes/src/com/revature/Launcher.java
+++ HelloScopes/src/com/revature/Launcher.java
@@ -6,7 +6,7 @@ package com.revature;
     -They can be accessed without an instance of the class.
     -Nonstatic members cannot be accessed this way, we must make an object before we can access them
 
--Nonstatic members cannot be access within static methods
+-Nonstatic members cannot be accessed within static methods
 
   */
 
@@ -28,6 +28,72 @@ public class Launcher {
         //Now, we can access int3!
         System.out.println(tester.int3);
 
+        System.out.println("==================(Manipulating ScopeTester's Variables)");
+
+        //instantiate a second ScopeTester object to demonstrate static/nonstatic behaviors
+        ScopeTester tester2 = new ScopeTester();
+
+        //above, we printed out int 1-3 from tester. Now let's print them out from tester2
+        System.out.println(tester2.int1);
+        System.out.println(tester2.int2);
+        System.out.println(tester2.int3);
+
+        //let's change these in tester2
+        tester2.int1 = 100;
+        tester2.int2 = 200;
+        tester2.int3 = 300;
+
+        //let's print out the variables from both tester and tester2
+        System.out.println("Tester 1: " + tester.int1);
+        System.out.println("Tester 2: " + tester2.int1);
+        //Since int1 is static, when we change it in one object, it changes for every object
+
+        //same story, int2 is static
+        System.out.println("Tester 1: " + tester.int2);
+        System.out.println("Tester 2: " + tester2.int2);
+
+        //int3 is nonstatic, so its value is unique to each object
+        System.out.println("Tester 1: " + tester.int3);
+        System.out.println("Tester 2: " + tester2.int3);
+
+        //for every static variable, the most recent value will be shared amongst every object.
+        //for non-static variables, only the object whose value you changed will change.
+
+        System.out.println("====================(Method/Block Scope)");
+
+        methodScopeExample(50);
+
+        //Not typically advised to make an object of a Class within that same Class
+        //But this would be the only way to get this method to run if we kept it non-static
+        Launcher launcher = new Launcher();
+        launcher.methodThatWontRunInMain();
+
+    }
+
+    public static void methodScopeExample(int x){
+
+        //this variable is METHOD SCOPED. only visible within its method
+        int methodScopedVariable = 10;
+
+        //parameters are also method scoped
+        System.out.println("The parameter " + x + " is method scoped!");
+
+        if(true){
+            //this variable is BLOCK SCOPED. only visible within its block
+            int blockScopedVariable = 20;
+
+            //we can access both of these variables here, since they're defined within this scope
+            System.out.println(methodScopedVariable);
+            System.out.println(blockScopedVariable);
+        }
+
+        //Java won't like this - we're trying to access this variable outside the scope it was defined in
+        //System.out.println(blockScopedVariable);
+
+    }
+
+    public void methodThatWontRunInMain(){
+        System.out.println("Hi I'm nonstatic, I won't be able to run in main UNLESS we made a Launcher object");
     }
 
 }
